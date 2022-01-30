e9fceb361b4170e5387ceff36dcacae524096a80
Simon <simpa92@icloud.com>
2022-01-30 21:53:14 +0100

    Add project files.

---
+++ Pages/Razor.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DT191G___Moment_1._2.Pages
+{
+    public class RazorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ wwwroot/js/main.js
@@ -0,0 +1,31 @@
+﻿"use strict";
+// HTML-element
+const logoEl = document.getElementById("logo");
+const dateEl = document.getElementById("datebox");
+const mainmenuEl = document.getElementById("mainnav");
+const headerEl = document.getElementById("mainheader");
+const containerEl = document.getElementById("content");
+
+// Eventhanterare triggad av skrollning
+window.onscroll = function () {
+    // Kontrollera om positionen är längre ner än toppen
+    if (scrollY > 0) {
+        // Anpassa sidhuvud och element
+        logoEl.style.width = "175px";
+        headerEl.style.position = "fixed";
+        headerEl.style.top = "0";
+        headerEl.style.width = "100vw";
+        mainmenuEl.style.display = "none";
+        dateEl.style.display = "none";
+        containerEl.style.marginTop = "100px";
+    } else {
+        // Återställ sidhuvud och element
+        logoEl.style.width = "";
+        headerEl.style.position = "";
+        headerEl.style.top = "";
+        headerEl.style.width = "";
+        mainmenuEl.style.display = "";
+        dateEl.style.display = "";
+        containerEl.style.marginTop = "";
+    }
+}
