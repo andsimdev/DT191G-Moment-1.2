251cbc47c4d9e5cb875e5111a147a07ece82f60b
Simon <simpa92@icloud.com>
2022-01-30 22:49:53 +0100

    Uppdateringar, mobilmeny med mera

--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -1,31 +1,37 @@
 ﻿"use strict";
-// HTML-element
-const logoEl = document.getElementById("logo");
-const dateEl = document.getElementById("datebox");
-const mainmenuEl = document.getElementById("mainnav");
-const headerEl = document.getElementById("mainheader");
-const containerEl = document.getElementById("content");
 
-// Eventhanterare triggad av skrollning
-window.onscroll = function () {
-    // Kontrollera om positionen är längre ner än toppen
-    if (scrollY > 0) {
-        // Anpassa sidhuvud och element
-        logoEl.style.width = "175px";
-        headerEl.style.position = "fixed";
-        headerEl.style.top = "0";
-        headerEl.style.width = "100vw";
-        mainmenuEl.style.display = "none";
-        dateEl.style.display = "none";
-        containerEl.style.marginTop = "100px";
+// HTML-element
+const menubtnEl = document.getElementById("mobilemenubtn");
+const menuulEl = document.getElementById("mobilemenuul");
+
+// Dölj mobilmenyn när JavaScript är tillgängligt (för mindre skärmar)
+if (window.matchMedia("(max-width: 900px)").matches) {
+    menuulEl.style.display = "none";
+    menubtnEl.innerHTML = "Meny";
+}
+
+// Kontrollera skärmbredd vid ändring av fönsterstorlek
+window.onresize = () => {
+    if (window.matchMedia("(max-width: 900px)").matches) {
+        menuulEl.style.display = "none";
+        menubtnEl.innerHTML = "Meny";
     } else {
-        // Återställ sidhuvud och element
-        logoEl.style.width = "";
-        headerEl.style.position = "";
-        headerEl.style.top = "";
-        headerEl.style.width = "";
-        mainmenuEl.style.display = "";
-        dateEl.style.display = "";
-        containerEl.style.marginTop = "";
+        menuulEl.style.display = "";
+    }
+}
+
+// Eventhanterare, lyssna efter klick på menyknappen
+menubtnEl.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+    // Kontrollera om menyn är dold
+    if (menuulEl.style.display == "none") {
+        // Visa menyn
+        menuulEl.style.display = "";
+        menubtnEl.innerHTML = "Stäng menyn";
+    } else {
+        //Dölj menyn
+        menuulEl.style.display = "none";
+        menubtnEl.innerHTML = "Meny";
     }
 }
